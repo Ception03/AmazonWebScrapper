8c5330493868be8c2661f8bba8febda30451e1f1
Ception03 <102429463+Ception03@users.noreply.github.com>
2023-07-04 15:16:54 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,86 +1,11 @@
-
-
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// const App = () => {
-//   const [input, setInput] = useState('');
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(null);
-
-//   const handleInputChange = (event) => {
-//     setInput(event.target.value);
-//   };
-
-//   const handleDownload = () => {
-//     if (input) {
-//       setLoading(true);
-//       setError(null);
-
-//       const options = {
-//         method: 'GET',
-//         url: 'https://amazon-web-scraping-api.p.rapidapi.com/products/search',
-//         params: {
-//           criteria: input,
-//           page: '10',
-//           countryCode: 'US',
-//           languageCode: 'EN',
-//         },
-//         headers: {
-//           'X-RapidAPI-Key': '74bd45b87cmsh43af6f12bd1d35ep12d165jsn91f877e1c3ff',
-//           'X-RapidAPI-Host': 'amazon-web-scraping-api.p.rapidapi.com',
-//         },
-//       };
-
-//       axios
-//         .request(options)
-//         .then((response) => {
-//           downloadJsonFile(response.data);
-//         })
-//         .catch((error) => {
-//           setError(error.message);
-//         })
-//         .finally(() => {
-//           setLoading(false);
-//         });
-//     }
-//   };
-
-//   const downloadJsonFile = (data) => {
-//     const filename = 'scraped_data.json';
-//     const jsonStr = JSON.stringify(data, null, 2);
-//     const blob = new Blob([jsonStr], { type: 'application/json' });
-//     const url = URL.createObjectURL(blob);
-//     const link = document.createElement('a');
-//     link.href = url;
-//     link.download = filename;
-//     document.body.appendChild(link);
-//     link.click();
-//     document.body.removeChild(link);
-//     URL.revokeObjectURL(url);
-//   };
-
-//   return (
-//     <div>
-//       <form>
-//         <input type="text" value={input} onChange={handleInputChange} placeholder="Enter Amazon product link" />
-//         <button type="button" onClick={handleDownload}>
-//           Scrape and Download JSON
-//         </button>
-//       </form>
-//       {loading && <p>Loading...</p>}
-//       {error && <p>An error occurred: {error}</p>}
-//     </div>
-//   );
-// };
-
 // export default App;
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
 
 const App = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -95,21 +20,22 @@ const App = () => {
 
       const scrapeData = async () => {
         const options = {
-          method: 'GET',
-          url: 'https://amazon-web-scraping-api.p.rapidapi.com/products/search',
+          method: "GET",
+          url: "https://amazon-web-scraping-api.p.rapidapi.com/products/search",
           params: {
             criteria: input,
-            countryCode: 'US',
-            languageCode: 'EN',
+            countryCode: "US",
+            languageCode: "EN",
           },
           headers: {
-            'X-RapidAPI-Key': '74bd45b87cmsh43af6f12bd1d35ep12d165jsn91f877e1c3ff',
-            'X-RapidAPI-Host': 'amazon-web-scraping-api.p.rapidapi.com',
+            "X-RapidAPI-Key":
+              "74bd45b87cmsh43af6f12bd1d35ep12d165jsn91f877e1c3ff",
+            "X-RapidAPI-Host": "amazon-web-scraping-api.p.rapidapi.com",
           },
         };
 
         let page = 1;
-        const totalPagesToScrape = 2; // Set the desired number of pages to scrape
+        const totalPagesToScrape = 200; // Set the desired number of pages to scrape
 
         const scrapedData = [];
 
@@ -141,11 +67,11 @@ const App = () => {
   };
 
   const downloadJsonFile = (data) => {
-    const filename = 'scraped_data.json';
+    const filename = "scraped_data.json";
     const jsonStr = JSON.stringify(data, null, 2);
-    const blob = new Blob([jsonStr], { type: 'application/json' });
+    const blob = new Blob([jsonStr], { type: "application/json" });
     const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
     link.download = filename;
     document.body.appendChild(link);
@@ -155,14 +81,22 @@ const App = () => {
   };
 
   return (
-    <div>
-      <form>
-        <input type="text" value={input} onChange={handleInputChange} placeholder="Enter Amazon product link" />
+    <div className="container">
+      <div className="logo">DSCRAP</div>
+      <h1>Dive Into The Power Of DSCRAP</h1>
+      <h3>Explore Amazon like never before with our advanced data scraping tool</h3>
+      <div className="content">
+        <input
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Paste Amazon product link here"
+        />
         <button type="button" onClick={handleScrape}>
-          Scrape and Download JSON
+          Download JSON
         </button>
-      </form>
-      {loading && <p>Loading...</p>}
+      </div>
+      {loading && <span className="loading-spinner"></span>}
       {error && <p>An error occurred: {error}</p>}
     </div>
   );
